8e70af2d1cdf0df8c91b6f3a67f3cecc193e09bf
Vityuk Yuriy <you.thunderstruck@gmail.com>
2024-02-01 17:59:19 +0200

    add readme

--- monte_carlo.py
+++ monte_carlo.py
@@ -12,15 +12,13 @@ def is_inside(a, b, x, y):
 
 
 def monte_c(counter=0, SmPrev=0, points=[]):
+    """Рахує площу фігури, після чого перевіряє рівень змін в порівнянні з попреедньою ітерацією. При невиконанні бажаних умов, запускається рекурсивно"""
     counter += 1
-    print(counter)
     points.extend([(random.uniform(0, a), random.uniform(0, b)) for _ in range(1000)])
     inside_points = [point for point in points if is_inside(a, b, point[0], point[1])]
 
     N = len(points)
     M = len(inside_points)
-    print(N)
-    print(M)
     SmCurrent = (M / N) * (a * b)  # Площа за методом Монте-Карло
     if SmCurrent - SmPrev > 0.00001 or SmCurrent - SmPrev < -(0.00001):
         return monte_c(counter, SmCurrent, points)
@@ -39,5 +37,12 @@ def f(x):
 S = result, error = spi.quad(f, 0, 2)  # Теоретична площа
 
 # Виведення результатів, зміненої моделі
-print(f"Теоретична площа фігури: {S}, площа за методом Монте-Карло: {Sm}")
-print(f"Точність до 0,00001 була досягнута за {rounds} раундів, кожен по 1000 точок.")
+
+with open("readme.md", "w", encoding="utf-8") as fp:
+    fp.writelines(
+        "Модель була змінена таким чином. Раунд додає 1000 точок у площину.\nВираховується площа. Якщо площа від попередньої ітерації відрізняється менше ніж задане число (в данному випадку 0.0001), тоді модель зупиняється, вважаючи що бажана точність досягнута.\n"
+    )
+    fp.writelines(f"Теоретична площа фігури: {S}, площа за методом Монте-Карло: {Sm}\n")
+    fp.writelines(
+        f"Точність до 0,00001 була досягнута за {rounds} раундів, кожен по 1000 точок.\n"
+    )
